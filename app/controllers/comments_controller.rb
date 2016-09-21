bdec15e14a54436160f733dab2e09567c6c0cddf
Halle Williams and James Turello <halle+james@>
2016-09-20 18:14:23 -0700

    add new/create comment views/controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,22 @@ class CommentsController < ApplicationController
     @comment = @question.comments.new(comment_params)
 
     if @comment.save
-      flash[:notice] = "Comment successfully added!"
-      redirect_to question_path @comment.question
+      respond_to do |format|
+        format.html do
+          flash[:notice] = "Comment successfully added!"
+          redirect_to question_path @comment.question
+        end
+        format.js
+      end
     else
-      flash[:alert] = "Comment not added. Try again!"
-      redirect_to new_question_comment_path @comment.question, @comment
+      respond_to do |format|
+        format.html do
+          flash[:alert] = "Comment not added. Try again!"
+          redirect_to new_question_comment_path @comment.question, @comment
+        end
+        puts 'js- comment save failed' + @comment.errors.inspect
+        format.js
+      end
     end
   end
 
